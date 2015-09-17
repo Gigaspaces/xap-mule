efda06d7eb46d888e31d071e2fc576c8f89beaee
Niv Ingberg <niv@gigaspaces.com>
2015-09-17 13:31:41 +0300

    GS-12549 - Removed SEDA integration Reviewer: Moran

--- mule-latest/src/test/java/org/openspaces/itest/esb/mule/queue/FlowBasedTxSimpleRollBackQueueTests.java
+++ mule-latest/src/test/java/org/openspaces/itest/esb/mule/queue/FlowBasedTxSimpleRollBackQueueTests.java
@@ -1,6 +1,16 @@
 package org.openspaces.itest.esb.mule.queue;
 
-public class FlowBasedTxSimpleRollBackQueueTests extends TxXASimpleRollBackQueueTests {
+import org.mule.api.MuleMessage;
+import org.openspaces.itest.esb.mule.AbstractMuleTests;
+
+public class FlowBasedTxSimpleRollBackQueueTests extends AbstractMuleTests {
+
+    public void testSimpleQueueHandling() throws Exception {
+        muleClient.dispatch("os-queue://test1", "testme", null);
+
+        MuleMessage message = muleClient.request("os-queue://test3", 5000);
+        assertEquals("testmeAppender1Appender2", message.getPayload());
+    }
 
     @Override
     protected String getConfigResources() {
